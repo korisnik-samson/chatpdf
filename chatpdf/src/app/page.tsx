52176729bc5df4be165e5b4e7471ad0dec8eb9af
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-11-23 13:35:56 +0100

    Added FileUploader.tsx

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
 
 export default async function Home() {
     const { userId } = await auth();
@@ -27,7 +28,7 @@ export default async function Home() {
                     </p>
 
                     <div className="w-full mt-4">
-                        {isAuth ? (<h1>fileUpload</h1>) : (
+                        {isAuth ? (<FileUpload />) : (
                             <Link href='/sign-in'>
                                 <Button>
                                     Login to get started!
